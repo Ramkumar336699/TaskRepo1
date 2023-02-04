0f3f3b554081a6212a75dcbc674be1dabba7c7e3
Ramkumar336699 <ramkumarpenkey3@gmail.com>
2023-02-04 10:07:26 +0530

    c.java is added

---
+++ c.java
@@ -0,0 +1,19 @@
+public class WrapperDemo {
+
+    public static void main(String[] args) {
+
+        // Primitive to String
+
+        int x= 123;
+//        String s1 = x;
+//        String s2= (String)x;
+        String s3 = x+"";
+        String s4 = String.valueOf(x);
+
+        // String to
+
+        String str1 = "123";
+        int y = Integer.parseInt(str1);
+
+    }
+}
