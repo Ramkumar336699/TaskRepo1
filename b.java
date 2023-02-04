676174002711cef47405d2c713ca50e2b0a1dc60
Ramkumar336699 <112113854+Ramkumar336699@users.noreply.github.com>
2023-02-04 09:51:35 +0530

    Very Good Ram Kumar..!

---
+++ b.java
@@ -0,0 +1,16 @@
+This is second program
+
+Things to Brush-up again:
+	1. Manual testing concepts
+	2. Dev related basic things
+	3. GIT
+	4. SQL
+	5. Manual API Testing concepts
+	6. REST Assured API Automation Testing
+	7. JAVA Concepts
+	8. Selenium UI Automation Testing
+	9. Basic Performance Testing
+ 10. Try to understand Resume Preparation and start doing
+ 11. Try to understand how to apply Good companies & follow-up
+
+Other upcoming testing areas (Azure Testing)
